a4d380042428084a96e196675c04f41de90d32dc
lalittoge <lalittoge57@gmail.com>
2026-06-13 22:18:16 +0530

    remove hash

--- assignment-1.py
+++ assignment-1.py
@@ -1,48 +1,48 @@
 # Question no 1:-
-# x=100
-# print(x)
-# print(type(x))
-# print(id(x))
+x=100
+print(x)
+print(type(x))
+print(id(x))
 
 # Question no 2:-
-# a=10
-# b=10
-# print(id(a))
-# print(id(b))
+a=10
+b=10
+print(id(a))
+print(id(b))
 
-# a=[10]
-# b=[10]
-# print(id(a))
-# print(id(b))
+a=[10]
+b=[10]
+print(id(a))
+print(id(b))
 
 # Question no 3:-
-# from sys import getsizeof
-# a=3
-# print(getsizeof(a))
-# b=3.14
-# print(getsizeof(b))
-# c="Lalit"
-# print(getsizeof(c))
-# d=[10,20,30,40]
-# print(getsizeof(d))
-# e=(10,20,60)
-# print(getsizeof(e))
+from sys import getsizeof
+a=3
+print(getsizeof(a))
+b=3.14
+print(getsizeof(b))
+c="Lalit"
+print(getsizeof(c))
+d=[10,20,30,40]
+print(getsizeof(d))
+e=(10,20,60)
+print(getsizeof(e))
 
 # Question no 4:-
 
 
 # Question no 5:-
-# a=10
-# b=10
-# print(id(a)==id(b))
+a=10
+b=10
+print(id(a)==id(b))
 
 # Question no 6:-
-# x=int(input("Enter the  1 st no"))
-# y=int(input("Enter the  2 st no"))
-# print("addition" ,x+y)
-# print("substraction" ,x-y)
-# print("multiplication" ,x*y)
-# print("division" ,x/y)
+x=int(input("Enter the  1 st no"))
+y=int(input("Enter the  2 st no"))
+print("addition" ,x+y)
+print("substraction" ,x-y)
+print("multiplication" ,x*y)
+print("division" ,x/y)
 
 
 # Question no 7:-
@@ -50,8 +50,8 @@
 
 
 # Question no 8:-
-# x=input("Enter the no")
-# print(type(x))
+x=input("Enter the no")
+print(type(x))
 
 # Question no 9:-
 
@@ -61,5 +61,5 @@ print( "Hellow", name, " , you will turn" ,age+1, "next year ")
 
 
 # Question no 10:-
-# print("10"+"20")
-# print(10+20)
+print("10"+"20")
+print(10+20)
